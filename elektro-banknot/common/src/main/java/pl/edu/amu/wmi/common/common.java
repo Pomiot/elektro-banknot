9ea30918a4f1f401868e13f198fe6e4af18be8f3
morfeusz2844 <patryk.augustyn@gmail.com>
2014-12-29 17:13:53 +0100

    Add skeleton of project, contain empty class modules

---
+++ elektro-banknot/common/src/main/java/pl/edu/amu/wmi/common/common.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.common;
+
+/**
+ *
+ * @author Patryk
+ */
+public class common {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.customer;
+
+/**
+ *
+ * @author Patryk
+ */
+public class customer {
+    public static void main(String[] args) {
+        
+    }
+    
+}

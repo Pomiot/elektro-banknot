1a152f940fd98b8001b57976a4cea817a5dc64eb
Tomasz Grzesiak <grzesiak.t@gmail.com>
2015-01-08 15:13:12 +0100

    Checking if jms communication works - and it does!

--- elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
+++ elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
@@ -3,10 +3,14 @@ package pl.edu.amu.wmi.customer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.edu.amu.wmi.customer.UI.UI;
+import pl.edu.amu.wmi.customer.services.CustomerCashGenerationService;
 
 public class Customer {
     public static void main(String[] args) {
-         ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+        CustomerCashGenerationService cgs = (CustomerCashGenerationService) context.getBean("customerCashGenerationService");
+
+        cgs.generateCash("DUUUUUUUUPA");
         
         System.out.println("Run customer application");
         UI ui = new UI();

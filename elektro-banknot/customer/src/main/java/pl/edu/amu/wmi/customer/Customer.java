126813a34fce10de61484087d93f25f4d7fd4cab
Tomasz Grzesiak <pomiot@gmail.com>
2015-01-08 14:45:01 +0100

    Update and rename customer.java to Customer.java

--- elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
+++ elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
@@ -2,8 +2,10 @@ package pl.edu.amu.wmi.customer;
 
 import pl.edu.amu.wmi.customer.UI.UI;
 
-public class customer {
+public class Customer {
     public static void main(String[] args) {
+         ApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+        
         System.out.println("Run customer application");
         UI ui = new UI();
         ui.start();

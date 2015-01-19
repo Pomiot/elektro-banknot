167cc439d8cbb9889d6ee94ff117b0a39aa49512
Tomasz Grzesiak <pomiot@gmail.com>
2015-01-19 22:42:41 +0100

    Update cashReceptionService.java

--- elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/services/cashReceptionService.java
+++ elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/services/cashReceptionService.java
@@ -1,7 +1,91 @@
 package pl.edu.amu.wmi.customer.services;
 
+import com.google.common.base.Preconditions;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import pl.edu.amu.wmi.common.objects.SignedBanknote;
+import pl.edu.amu.wmi.common.objects.UnblindingKeysResponse;
+import pl.edu.amu.wmi.common.objects.UnblindingKeysRequest;
+
+import javax.jms.*;
+
 /**
  * Created by Tomasz on 2015-01-15.
  */
-public class cashReceptionService {
+public class CashReceptionService implements MessageListener {
+
+    public Destination getCashReceptionQueue() {
+        return cashReceptionQueue;
+    }
+
+    public void setCashReceptionQueue(Destination cashReceptionQueue) {
+        this.cashReceptionQueue = cashReceptionQueue;
+    }
+
+    public JmsTemplate getJmsTemplate() {
+        return jmsTemplate;
+    }
+
+    public void setJmsTemplate(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    private JmsTemplate jmsTemplate;
+
+    private Destination cashReceptionQueue;
+
+    @Override
+    public void onMessage(final Message message) {
+        Preconditions.checkArgument(message instanceof ObjectMessage);
+
+        ObjectMessage objectMessage = (ObjectMessage) message;
+
+        try {
+            if (objectMessage.getObject() instanceof UnblindingKeysRequest) {
+
+                /**
+                 * Klient otrzymuje żądanie przesłania do banku kluczy odtajniających dla 99 banknotów
+                 * TODO: cała reszta
+                 */
+
+                UnblindingKeysRequest unblindingKeysRequest = (UnblindingKeysRequest) objectMessage.getObject();
+
+                System.out.println("Klient: otrzymałem od banku żądanie przekazania kluczy.");
+
+                jmsTemplate.send(objectMessage.getJMSReplyTo(), new MessageCreator() {
+                    @Override
+                    public Message createMessage(Session session) throws JMSException {
+
+                        ObjectMessage replyMessage = session.createObjectMessage();
+                        UnblindingKeysResponse unblindingKeysResponse = new UnblindingKeysResponse();
+
+                        replyMessage.setObject(unblindingKeysResponse);
+                        replyMessage.setJMSReplyTo(cashReceptionQueue);
+
+                        System.out.println("Klient: wysyłam do banku zestaw kluczy.");
+
+                        return replyMessage;
+                    }
+                });
+
+            }
+
+            if (objectMessage.getObject() instanceof SignedBanknote) {
+
+                /**
+                 * Klient otrzymuje podpisany banknot
+                 * TODO: cała reszta
+                 */
+
+                SignedBanknote signedBanknote = (SignedBanknote) objectMessage.getObject();
+
+                System.out.println("Klient: otrzymałem podpisany banknot.");
+
+            }
+
+
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
 }

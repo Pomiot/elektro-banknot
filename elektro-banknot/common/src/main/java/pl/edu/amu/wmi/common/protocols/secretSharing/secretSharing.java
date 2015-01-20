3c89abf1eada9c62485831be6b7105bb422d43a1
morfeusz2844 <patryk.augustyn@gmail.com>
2015-01-20 01:22:39 +0100

    Implemention banknote v1.1 Add implementation hashCommitmentScheme

--- elektro-banknot/common/src/main/java/pl/edu/amu/wmi/common/protocols/secretSharing/secretSharing.java
+++ elektro-banknot/common/src/main/java/pl/edu/amu/wmi/common/protocols/secretSharing/secretSharing.java
@@ -3,39 +3,46 @@ package pl.edu.amu.wmi.common.protocols.secretSharing;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.SecureRandom;
-import java.util.Arrays;
+import pl.edu.amu.wmi.common.Util.util;
 
 public class secretSharing {
 
     private String message;
-    private byte[] raw;
+    private byte[] byteMessage;
     private byte[] result;
-    
-    
+    private byte[] random1;
+
     public secretSharing(String message) {
         this.message = message;
     }
 
+    public secretSharing(byte[] byteMessage) {
+        this.message = new String();
+        this.byteMessage = byteMessage;
+
+    }
+
     @SuppressWarnings("empty-statement")
     public void generateSecretSharing() throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchProviderException {
-        //this.raw = message.getBytes("UTF8");
-        this.raw = new byte[]{0,0,0,0};
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-        System.out.println("Dlugosc raw:"+this.raw.length);
-        //byte[] r1 = new byte[this.raw.length];
-        byte[] r1 = new byte[]{1,1,1,1};
-        System.out.println("Dlugosc r1:"+r1.length);
-        //random.nextBytes(r1);
-        System.out.println(Arrays.toString(r1));
-        this.result = new byte[this.raw.length];
-        int i=0;
-        for (byte b: this.raw){
-            this.result[i]=(byte) (b^r1[i]);
+        if (!this.message.isEmpty()) {
+            this.byteMessage = message.getBytes("UTF8");
+        }
+        System.out.println("Dlugosc raw:" + this.byteMessage.length);
+        this.random1 = util.generateSecureRandom(this.byteMessage.length);
+        this.result = new byte[this.byteMessage.length];
+        int i = 0;
+        for (byte b : this.byteMessage) {
+            this.result[i] = (byte) (b ^ this.random1[i]);
             i++;
         }
-        System.out.println("Wyjscie:"+Arrays.toString(this.result));
-        
-        
     }
+
+    public byte[] getRandom1() {
+        return random1;
+    }
+
+    public byte[] getResult() {
+        return result;
+    }
+
 }

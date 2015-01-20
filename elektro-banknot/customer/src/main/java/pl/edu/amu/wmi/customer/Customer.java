c817a8994c0f9a468f021184038497c5139fa556
Tomasz Grzesiak <grzesiak.t@gmail.com>
2015-01-21 00:27:40 +0100

    Added bank sending key to customer

--- elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
+++ elektro-banknot/customer/src/main/java/pl/edu/amu/wmi/customer/Customer.java
@@ -2,8 +2,11 @@ package pl.edu.amu.wmi.customer;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.edu.amu.wmi.customer.services.BankPublicKeyReceiverService;
 import pl.edu.amu.wmi.customer.services.CustomerCashGenerationService;
 import pl.edu.amu.wmi.customer.services.PurchaseFromShopService;
+
+import java.security.PublicKey;
 
 public class Customer {
     public static void main(String[] args) throws InterruptedException {
@@ -11,9 +14,16 @@ public class Customer {
         CustomerCashGenerationService cgs = (CustomerCashGenerationService) context.getBean("customerCashGenerationService");
         PurchaseFromShopService pfs = (PurchaseFromShopService) context.getBean("purchaseFromShopService");
 
-        cgs.generateCash("DUUUUUUUUPA");
+        BankPublicKeyReceiverService keyReceiverService = (BankPublicKeyReceiverService) context.getBean("bankPublicKeyReceiverService");
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
+
+        PublicKey bankPublicKey = null;
+
+        bankPublicKey = (PublicKey) keyReceiverService.getBankPublicKey();
+
+
+        cgs.generateCash("DUUUUUUUUPA");
 
         pfs.makePurchase();
         
